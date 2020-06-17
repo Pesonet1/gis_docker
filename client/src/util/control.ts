16236e2a52b8b4c2da552ec0dda48fab37b5592c
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-06-17 13:20:57 +0300

    fix lint errors

--- client/src/util/control.ts
+++ client/src/util/control.ts
@@ -3,7 +3,10 @@ import { createStringXY } from 'ol/coordinate';
 
 import projection from './projection';
 
-export const mousePositionControl = (mousePositionElement: HTMLElement, coordinateDecimals: number): MousePosition => (new MousePosition({
+export const mousePositionControl = ( // eslint-disable-line
+  mousePositionElement: HTMLElement,
+  coordinateDecimals: number,
+): MousePosition => (new MousePosition({
   coordinateFormat: createStringXY(coordinateDecimals),
   projection,
   className: 'custom-mouse-position',
