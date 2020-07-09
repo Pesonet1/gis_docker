c5986bc7095facb7901e5f83f15494d5ec27ad8b
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-07-09 21:36:44 +0300

    fix sequelize test user seed

--- server/seeders/20200526172558-test-user.js
+++ server/seeders/20200526172558-test-user.js
@@ -9,6 +9,8 @@ module.exports = {
       password: '$2a$10$.Rk.gkjvpmf1zHxCypgfp.HC1qDcJYsec2RjD55Ij/KX3WE7qMQ0i',
       email: 'testi@testi.com',
       email_verified: true,
+      createdAt: new Date,
+      updatedAt: new Date,
     }], {});
   },
 
