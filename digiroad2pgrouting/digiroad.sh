9fe8d2bd6f471a50c6985f1434e33df9007739e7
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-10-05 08:17:57 +0300

    use env variables for import tools database connections

--- digiroad2pgrouting/digiroad.sh
+++ digiroad2pgrouting/digiroad.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis -tAc "CREATE SCHEMA IF NOT EXISTS routing_digiroad"
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis -tAc "DROP TABLE IF EXISTS routing_digiroad.digiroad"
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis -tAc "DROP TABLE IF EXISTS routing_digiroad.digiroad_vertices_pgr"
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis -tAc "DROP TABLE IF EXISTS routing_digiroad.digiroad_nopeusrajoitus"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME -tAc "CREATE SCHEMA IF NOT EXISTS $POSTGRES_IMPORT_SCHEMA"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME -tAc "DROP TABLE IF EXISTS $POSTGRES_IMPORT_SCHEMA.digiroad"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME -tAc "DROP TABLE IF EXISTS $POSTGRES_IMPORT_SCHEMA.digiroad_vertices_pgr"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME -tAc "DROP TABLE IF EXISTS $POSTGRES_IMPORT_SCHEMA.digiroad_nopeusrajoitus"
 
 for dir in digiroad_data/* ; do
   ogr2ogr -update -append -a_srs EPSG:3067 \
-    -nlt LINESTRING -lco GEOMETRY_NAME=geom -lco FID=id -lco SCHEMA=routing_digiroad \
-    -f PostgreSQL "PG:host=localhost port=5435 user=postgres dbname=gis password=postgres" $dir/DR_LINKKI_K.shp \
-    -nln routing_digiroad.digiroad
+    -nlt LINESTRING -lco GEOMETRY_NAME=geom -lco FID=id -lco SCHEMA=$POSTGRES_IMPORT_SCHEMA \
+    -f PostgreSQL "PG:host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USERNAME dbname=$POSTGRES_DBNAME password=$POSTGRES_PASSWORD" $dir/DR_LINKKI_K.shp \
+    -nln $POSTGRES_IMPORT_SCHEMA.digiroad
 
   ogr2ogr -update -append -a_srs EPSG:3067 \
-    -nlt LINESTRING -lco GEOMETRY_NAME=geom -lco FID=id -lco SCHEMA=routing_digiroad \
-    -f PostgreSQL "PG:host=localhost port=5435 user=postgres dbname=gis password=postgres" $dir/DR_NOPEUSRAJOITUS_K.shp \
-    -nln routing_digiroad.digiroad_nopeusrajoitus
+    -nlt LINESTRING -lco GEOMETRY_NAME=geom -lco FID=id -lco SCHEMA=$POSTGRES_IMPORT_SCHEMA \
+    -f PostgreSQL "PG:host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USERNAME dbname=$POSTGRES_DBNAME password=$POSTGRES_PASSWORD" $dir/DR_NOPEUSRAJOITUS_K.shp \
+    -nln $POSTGRES_IMPORT_SCHEMA.digiroad_nopeusrajoitus
 done
 
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis < sql/pgrouting_setup.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME < sql/pgrouting_setup.sql
--- osm2pgrouting/pgrouting.sh
+++ osm2pgrouting/pgrouting.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
+$OSM_FILE
+
+for entry in osm_data/*.osm.pbf; do
+  if [ -e $entry ]
+  then
+    OSM_FILE=$entry
+    break
+  fi
+done
+
+if [ -z $OSM_FILE ]
+then
+  echo "OSM file cannot be found from mounted volume. Exiting..."
+  exit
+fi
+
+echo "Using mounted $OSM_FILE OSM file"
+
 # Convert packed osm data into unpacked form
-osmconvert osm_data/$OSM_FILENAME_VARIABLE \
+osmconvert $OSM_FILE \
   --drop-author \
   --drop-version \
   --out-osm \
   -o=pgrouting_data.osm
 
 # Create "routing_osm" schema that is also used by digiroad2pgrouting
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis -tAc "CREATE SCHEMA IF NOT EXISTS routing_osm"
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME -tAc "CREATE SCHEMA IF NOT EXISTS $POSTGRES_IMPORT_SCHEMA"
 
 # Run osm2pgrouting
 osm2pgrouting \
   --f pgrouting_data.osm \
   --conf mapconfig.xml \
-  --dbname gis \
-  --schema routing_osm \
-  --host localhost \
-  --port 5435 \
-  --username postgres \
-  --password postgres \
+  --dbname $POSTGRES_DBNAME \
+  --schema $POSTGRES_IMPORT_SCHEMA \
+  --host $POSTGRES_HOST \
+  --port $POSTGRES_PORT \
+  --username $POSTGRES_USERNAME \
+  --password $POSTGRES_PASSWORD \
   --clean
 
 # Converting osm data srid from ESPG:4326 into EPSG:3067
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis < sql/srid_convert.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME < sql/srid_convert.sql
 
 # Creating views for routing functions
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis < sql/views.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME < sql/views.sql
 
 # Creating routing functions
-PGPASSWORD=postgres psql -U postgres -h localhost -p 5435 gis < sql/functions.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USERNAME -h $POSTGRES_HOST -p $POSTGRES_PORT $POSTGRES_DBNAME < sql/functions.sql
